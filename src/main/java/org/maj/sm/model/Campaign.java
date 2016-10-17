bda7ea85d7ea7a60a1a467edd20944110e151556
David Ashirov <David.Ashirov@iacapps.com>
2016-10-17 10:29:59 -0400

    Fixed the range mapped values, broke the test : ofy does not know how to store enums into the object database.

--- src/main/java/org/maj/sm/model/Campaign.java
+++ src/main/java/org/maj/sm/model/Campaign.java
@@ -1,9 +1,11 @@
 package org.maj.sm.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import org.maj.sm.model.enums.CampaignStatus;
 import org.maj.sm.utility.ChangeLogEntryComparator;
 
 import com.googlecode.objectify.annotation.Entity;
@@ -14,8 +16,11 @@ public class Campaign {
 
 	@Id public String code;
 	public String description;
+    public Long marketplace;  // where do we buy the traffc from
+    public String product; // what product are we advertising
 
     SortedSet<ChangeLogEntry<Double>> priceChangeLog = new TreeSet<>(new ChangeLogEntryComparator());
+    SortedSet<ChangeLogEntry<CampaignStatus>> statusChangeLog = new TreeSet<>(new ChangeLogEntryComparator());
 	
 	public Campaign() {
 		super();
@@ -27,14 +32,98 @@ public class Campaign {
 		this.description = description;
 	}
 	
-	public void addPrice(Date date, Double price){
-		this.priceChangeLog.add(new ChangeLogEntry(date,price)
+	public void setPrice(Date effectiveDate, Double campaignPrice){
+		this.priceChangeLog.add(new ChangeLogEntry(effectiveDate,campaignPrice)
 		);
 	}
 
-	@Override
-	public String toString() {
-		return "Campaign [code=" + code + ", description=" + description + ", price=" + this.priceChangeLog.last().getValue() + "]";
-	}
-	
+	public void setStatus(Date effectiveDate, CampaignStatus campaignStatus){
+        this.statusChangeLog.add(new ChangeLogEntry<CampaignStatus>(effectiveDate,campaignStatus));
+    }
+
+    public CampaignStatus getStatus(){
+        return getStatus(new Date());
+    }
+
+    public CampaignStatus getStatus(Date date) {
+        for (ChangeLogEntry<CampaignStatus> changeLogEntry: statusChangeLog
+             ) {
+            if (changeLogEntry.getEffectiveDate().before(date) || changeLogEntry.getEffectiveDate().equals(date)){
+                return changeLogEntry.getValue();
+            }
+
+        }
+        return CampaignStatus.DELETED;
+    }
+
+    public Double getPrice(){
+        return getPrice(new Date());
+    }
+
+    public Double getPrice(Date date){
+        /**
+         * Price is more than zero if campaign is active on the same day
+         */
+        if (!getStatus(date).equals(CampaignStatus.ACTIVE)){
+            return 0.0;
+        }
+        /**
+         * Now that we know it was active, find the price
+         */
+        for (ChangeLogEntry<Double> changeLogEntry: priceChangeLog
+             ) {
+            if (changeLogEntry.getEffectiveDate().before(date) || changeLogEntry.getEffectiveDate().equals(date)){
+                return changeLogEntry.getValue();
+            }
+        }
+        return 0.0;
+    }
+
+    public String statusChangeLogAsString(){
+        StringBuilder sb = new StringBuilder();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        sb.append("PriceChangeLog[");
+        for (ChangeLogEntry<CampaignStatus> e: statusChangeLog
+                ) {
+            sb.append("{");
+            sb.append(df.format(e.getEffectiveDate()));
+            sb.append(": ");
+            sb.append(e.getValue().toString());
+            sb.append("}");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public String priceChangeLogAsString(){
+        StringBuilder sb = new StringBuilder();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        sb.append("PriceChangeLog[");
+        for (ChangeLogEntry<Double> e: priceChangeLog
+             ) {
+            sb.append(" {");
+            sb.append(df.format(e.getEffectiveDate()));
+            sb.append(": ");
+            sb.append(e.getValue().toString());
+            sb.append("} ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+    
+    @Override
+    public String toString() {
+        return "Campaign{" +
+                "code='" + code + '\'' +
+                ", description='" + description + '\'' +
+                ", marketplace=" + marketplace +
+                ", product='" + product + '\'' +
+
+                ", price=" + this.getPrice(new Date()).toString() +
+                ", priceChangeLog=" + this.priceChangeLogAsString()+
+
+                ", status=" + this.getStatus(new Date()).toString() +
+                ", statusChangeLog=" + this.statusChangeLogAsString() +
+                '}';
+    }
 }
--- src/test/java/org/maj/sm/CampaignTest.java
+++ src/test/java/org/maj/sm/CampaignTest.java
@@ -18,6 +18,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.maj.sm.model.Campaign;
+import org.maj.sm.model.Marketplace;
+import org.maj.sm.model.enums.CampaignStatus;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
@@ -49,8 +55,41 @@ public class CampaignTest {
     
     @Test
     public void doTest() {
-        Campaign campaign = new Campaign("XX","Test Code");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dt1 = new Date();
+        Date dt2 = new Date();
+        Date dt3 = new Date();
+        try {
+            dt1 = dateFormat.parse("2001-01-01");
+            dt2 = dateFormat.parse("2001-01-02");
+            dt3 = dateFormat.parse("1999-03-01");
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+        Marketplace google = new Marketplace();
+        google.name="Google AdWords Market";
 
+        Campaign campaign = new Campaign("XX","Test Code");
+        campaign.marketplace = google.id;
+        campaign.setPrice(dt1, 15.0009);
+        System.out.println(campaign.toString());
+        Assert.assertEquals("Expected DELETED Campaign to show $0.00 Price", 0x0, campaign.getPrice().doubleValue(),0);
+
+        campaign.setStatus(dt1, CampaignStatus.ACTIVE );
+        System.out.println(campaign.toString());
+        Assert.assertEquals("Expected ACTIVE Campaign to show $15.0009 Price", 15.0009, campaign.getPrice().doubleValue(),0.000001);
+
+        campaign.setPrice(dt2, 10.0001);
+        System.out.println(campaign.toString());
+        Assert.assertEquals("Expected ACTIVE Campaign to show $10.0001 Price", 10.0001, campaign.getPrice().doubleValue(),0.000001);
+
+        campaign.setStatus(dt2, CampaignStatus.PAUSED );
+        System.out.println(campaign.toString());
+        Assert.assertEquals("Expected PAUSED Campaign to show $0.00 Price", 0x0, campaign.getPrice().doubleValue(),0);
+        Assert.assertEquals("Expected ACTIVE on " + dateFormat.format(dt1) +" Campaign to show $15.0009 Price", 15.0009, campaign.getPrice(dt1).doubleValue(),0.000001);
+        Assert.assertEquals("Expected DELETED on " + dateFormat.format(dt3) + "Campaign to show $0.00 Price", 0x0 , campaign.getPrice(dt3).doubleValue(),0);
+
+        System.out.println(campaign.toString());
         // 1) Save Campaign to data store
         ofy().save().entity(campaign).now();
 
